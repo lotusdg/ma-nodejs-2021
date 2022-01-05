95819192b103802bc36e54e44f1408995ab80521
lotusdg <yury.vlas@gmail.com>
2022-01-05 23:05:34 +0200

    add func createProduct && getProduct

--- src/app.js
+++ src/app.js
@@ -1,29 +1,39 @@
 const { db: dbConfig } = require('./config');
-const server = require('./server');
+// const server = require('./server');
 const db = require('./db')(dbConfig);
 
-function enableGracefulExit() {
-  const exitHandler = (error) => {
-    if (error) console.error(error);
-    console.log('Gracefully stopping...');
-    server.stop(() => {
-      process.exit();
-    });
-  };
+// function enableGracefulExit() {
+//   const exitHandler = (error) => {
+//     if (error) console.error(error);
+//     console.log('Gracefully stopping...');
+//     server.stop(() => {
+//       process.exit();
+//     });
+//   };
 
-  process.on('SIGINT', exitHandler);
-  process.on('SIGTERM', exitHandler);
-  process.on('SIGUSR1', exitHandler);
-  process.on('SIGUSR2', exitHandler);
-  process.on('uncaughtException', exitHandler);
-  process.on('unhandledRejection', exitHandler);
-}
+//   process.on('SIGINT', exitHandler);
+//   process.on('SIGTERM', exitHandler);
+//   process.on('SIGUSR1', exitHandler);
+//   process.on('SIGUSR2', exitHandler);
+//   process.on('uncaughtException', exitHandler);
+//   process.on('unhandledRejection', exitHandler);
+// }
 
-function boot() {
-  const t = db.testConnection();
-  console.log(t);
+async function boot() {
   // enableGracefulExit();
   // server.start();
+  // await db.testConnection();
+  const createProduct = await db.createProduct({
+    item: 'watermelon',
+    type: 'Mini Piccolo',
+    measure: 'Giant',
+    measureValue: 46,
+    priceType: 'pricePerItem',
+    priceValue: '$8',
+  });
+  const getProductByID = await db.getProduct(1);
+  // console.log(createProduct);
+  // console.log(getProductByID);
 }
 
 boot();
--- src/db/index.js
+++ src/db/index.js
@@ -5,13 +5,75 @@ module.exports = (config) => {
   return {
     testConnection: async () => {
       try {
-        console.log('hello from testconnection');
-        await client.query('SELECT NOW()');
+        console.log('hello from pg test connection');
+        const timeFromDb = await client.query('SELECT NOW()');
+        console.log(timeFromDb.rows[0]);
       } catch (err) {
         console.error(err.message || err);
         throw err;
       }
     },
+
+    createProduct: async ({
+      item,
+      type,
+      measure,
+      measureValue,
+      priceType,
+      priceValue,
+    }) => {
+      try {
+        if (
+          !item ||
+          !type ||
+          !measure ||
+          !measureValue ||
+          !priceType ||
+          !priceValue
+        ) {
+          throw new Error('ERROR: Product is not defined');
+        }
+
+        const timestamp = new Date();
+
+        const result = await client.query(
+          'INSERT INTO Products(item, type, measure, measurevalue, pricetype, pricevalue, createdate, datelastchange, deletedate) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
+          [
+            item,
+            type,
+            measure,
+            measureValue,
+            priceType,
+            priceValue,
+            timestamp,
+            timestamp,
+            null,
+          ],
+        );
+
+        return `DEBUG: New product created: ${JSON.stringify(result.rows[0])}`;
+      } catch (err) {
+        console.error(err.message || err);
+        throw err;
+      }
+    },
+
+    getProduct: async (id) => {
+      try {
+        if (!id) {
+          throw new Error('ERROR: Product is not defined');
+        }
+        const result = await client.query(
+          'SELECT * FROM Products p WHERE p.id = $1 AND p.deletedate IS NULL',
+          [id],
+        );
+        return result.rows[0];
+      } catch (err) {
+        console.error(err.message || err);
+        throw err;
+      }
+    },
+
     close: async () => {
       console.log('close pg db connection');
       client.end();
